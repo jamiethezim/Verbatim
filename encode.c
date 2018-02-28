8ab9c0b08ab147b9b489c62564ba62fe440d55fd
Jamie Zimmerman <jamiez@uoregon.edu>
2018-02-27 17:24:24 -0800

    strings are hard:(

--- encode.c
+++ encode.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <assert.h>
 
-int getwordlen(char * s){
+int getwordlen(unsigned char * s){
 	// while the number of bits % 3 != 0
 	// tack more zeros onto the front
-	char* p = s;
+	unsigned char* p = s;
 	while (*p != '\0'){
 		p++;
 	}
 	return (p-s);
 }
 
-long long change_format(char* s){
+long long change_format(unsigned char* s){
 	int wordlen = getwordlen(s);
 	if (wordlen > 8){
 		fprintf(stderr, "Error: %s has length greater than 8, please supply word where length less than or equal to 8\n", s);
@@ -47,39 +47,64 @@ unsigned char * getencodingscheme(){
 };
 
 
-void translate(char final_word[23], unsigned char* scheme, long long word, int wordlength){
+void translate(unsigned char final_word[23], unsigned char* scheme, long long word, int wordlength){
 	long long three_bit_mask = 0xd000000000000000; // three bits in the 61st-63rd bit
 	//char final_word[23] = {'\0'}; // a word of length 8 will have 64 bits, which will get encoded to ceil(64/3) = 22 letters
-	for (int i = 0; i < wordlength; i++){
+
+	if (wordlength == 8){
+
+	}
+		// we have a special case where all 64 bits are used to represent the word
+		// but we need two extra bits at the front to be able to interpret the bits three-by-three (66 % 3 = 0)
+		// so now we have to handle this very carefully
+		// read the last 63 bits backwards to get them into translated/encoded chars
+		// then for the 64th bit, create a new temp char that has two zeros in front of the 64th bit
+		// stuff that temp char to the front of the encoded word
+	int i = 0; 
+	while (i < wordlength*8/3){
 		int letter = (int) (word & three_bit_mask) >> ((64-3)-i*3);
 		final_word[i] = scheme[letter];
 		three_bit_mask = three_bit_mask >> 3;
+		i += 3;
 	}
-	
 
 }
 
-/*
-unsigned char* fresh_encode(unsigned char * this_word){
-	return "ok";
-}
 
 int test(){
+	unsigned char* schema;
+	schema = getencodingscheme();
 	fprintf(stderr, "****** TESTING 3 cases **********\n");
 	// 106, 97, 109, 105, 101 -> 1101010, 1100001, 1101101, 1101001, 1100101 ->
 	// encode(11010101100001110110111010011100101) -> brtaeiimrbat
-	assert(fresh_encode("jamie") == "brtaeiimrbat"); 
-	assert(fresh_encode("5!") == "vbriaae"); //53, 33 -> 00110101, 00100001 -> encode(000011010100100001) -> vbriaae
+	unsigned char* new_word = (unsigned char *) malloc(sizeof(unsigned char) * 23);
+	long long formatted = 0;
+
+	// test 1
+	formatted = change_format("jamie\0");
+	translate(new_word, schema, formatted, 5);
+ 	assert(new_word == "brtaeiimrbat"); 
+
+ 	// test 2
+ 	formatted = change_format("5!\0");
+ 	translate(new_word, schema, formatted, 2);
+	assert(new_word == "vbriaae"); //53, 33 -> 00110101, 00100001 -> encode(000011010100100001) -> vbriaae
+
+	// test 3
+	formatted = change_format("||\0");
+	translate(new_word, schema, formatted, 2);
 	assert(fresh_encode("||") == "vmiema"); // 124, 124 -> 1111100, 1111100 -> encode(000111110001111100) -> vmiema
+
 	fprintf(stderr, "*Done with test cases... add some more?\n");
 
+	return 0;
 }
-*/
+
 int main(int argc, char* argv[]){
 	char* t = argv[1];
-	long long test = change_format(t);
-	fprintf(stdout, "new word is %llu\n", test);
+	long long baby = change_format(t);
+	fprintf(stdout, "new word is %llu\n", baby);
 
-	//test() //uncomment to run unit tests
+	test(); //uncomment to run unit tests
 	//encode_file() // Uncomment when ready to encode a story!
 }	
